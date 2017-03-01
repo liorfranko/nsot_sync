6116c366299719f87568d8ce58d9dfb4ea78b746
liorf <liorf@liveperson.com>
2017-03-01 23:03:32 +0200

    replace snmp basestring to str

--- nsot_sync/commands/device_scanner.py
+++ nsot_sync/commands/device_scanner.py
@@ -12,8 +12,8 @@ from nsot_sync.drivers import device_scanner
     help='Maximum threads for the network scan, default is 100.'
 )
 @click.option('--scan-vlan', type=int, required=True, help='The vlan ID of the management vlan.')
-@click.option('--snmp-community', type=basestring, required=True, help='The SNMP community for auto discovery.')
-@click.option('--snmp-version', type=basestring, required=True, default='v2c', help='The SNMP version for auto discovery.')
+@click.option('--snmp-community', type=str, required=True, help='The SNMP community for auto discovery.')
+@click.option('--snmp-version', type=str, required=True, default='v2c', help='The SNMP version for auto discovery.')
 @click.pass_context
 def cli(ctx, max_threads, scan_vlan, snmp_community, snmp_version):
     driver = device_scanner.DeviceScannerDriver(
