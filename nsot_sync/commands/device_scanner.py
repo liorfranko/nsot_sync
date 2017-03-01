cd03da14db62a1fff1456dda84848d600d8b8624
liorf <liorf@liveperson.com>
2017-03-01 23:40:30 +0200

    change exit to raise in creds_mng

--- nsot_sync/commands/device_scanner.py
+++ nsot_sync/commands/device_scanner.py
@@ -11,18 +11,31 @@ from nsot_sync.drivers import device_scanner
     type=int,
     help='Maximum threads for the network scan, default is 100.'
 )
-@click.option('--scan-vlan', type=int, required=True, help='The vlan ID of the management vlan.')
-@click.option('--snmp-community', type=str, required=True, help='The SNMP community for auto discovery.')
-# @click.option('--update-creds', is_flag=True, default=False, help='Update the exiting cached credentials.')
+@click.option(
+    '--scan-vlan',
+    type=int,
+    required=True,
+    help='The vlan ID of the management vlan.')
+@click.option(
+    '--snmp-community',
+    type=str,
+    required=True,
+    help='The SNMP community for auto discovery.')
+@click.option(
+    '--snmp-version',
+    type=str,
+    required=True,
+    help='The SNMP version for auto discovery.')
+@click.option('--update-creds', is_flag=True, default=False, help='Update the exiting cached credentials.')
 @click.pass_context
-def cli(ctx, max_threads, scan_vlan, snmp_community, snmp_version):
+def cli(ctx, max_threads, scan_vlan, snmp_community, snmp_version, update_creds):
     driver = device_scanner.DeviceScannerDriver(
         click_ctx=ctx,
         max_threads=max_threads,
         scan_vlan=scan_vlan,
         snmp_community=snmp_community,
         snmp_version=snmp_version,
-        # update_creds=update_creds
+        update_creds=update_creds
     )
     if ctx.obj['NOOP']:
         driver.noop()
